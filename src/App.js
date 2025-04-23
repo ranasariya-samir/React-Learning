2cb00f5fcca0eaab28fea0adbdb6bb3fb890517a
Ranasariya Samir <ranasariyasamir86@gmail.com>
2025-04-23 13:34:26 +0530

    scond commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Col, Container, Row,Card ,Button} from 'react-bootstrap';
+import Header from './Header';
+import Footer from './Footer';
+import { blog } from './Data/blog';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Container>
+        <Header></Header>
+      </Container>
+      <Container>
+        <Row>
+          {blog.map((v,i)=>{
+            return(
+              <ProductItem pitem = {v} key= {i}></ProductItem>
+            )
+          })}
+        </Row>
+        {/* <ProductItem></ProductItem> */}
+      </Container>
+      <Container>
+        <Footer></Footer>
+      </Container>
+    </div>
   );
 }
 
+
 export default App;
+
+function ProductItem({pitem}) {
+  return(
+    <div className="col-lg-3 mb-4">
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{pitem.title}</Card.Title>
+          <Card.Text>
+            {pitem.body}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div>
+            <h1>Footer part</h1>
+        </div>
+    )
+}
